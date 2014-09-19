ae097d7f45742ae81d8246abf9c0e3dff56aedb1
mbrown101 <m.g.brown85@gmail.com>
2014-09-19 10:28:11 -0700

    Update stormData.R

--- stormData.R
+++ stormData.R
@@ -32,6 +32,9 @@ library(ggplot2)
 install.packages("R.utils")
 library(R.utils)
 
+install.packages("reshape")
+library(reshape)
+
 ### Loading and preprocessing the data ###
 
 # import and unzip data
@@ -44,13 +47,47 @@ bunzip2(paste(getwd() , "/repdata-data-StormData.csv.bz2" , sep = '' ) , overwri
 data <- as.data.frame(read.csv(paste(getwd() , "/repdata-data-StormData.csv" , sep = '' )))
 colnames(data) <- make.names(colnames(data) , allow_ = FALSE)
 
-# remove columns not in initial investigation
-drop.columns <- c( 'STATE..' , 'COUNTY' , 'TIME.ZONE' , 'REFNUM' , 'REMARKS' , 'BGN.AZI' , 'COUNTYENDN' , 'BGN.RANGE' , 'COUNTY.END' , 'END.RANGE' , 'END.AZI' , 'WFO' , 'STATEOFFIC')
+# remove selected columns for initial investigation
+#drop.columns <- c( 'STATE..' , 'COUNTY' , 'TIME.ZONE' , 'REFNUM' , 'REMARKS' , 'BGN.AZI' , 'COUNTYENDN' , 'BGN.RANGE' , 'COUNTY.END' , 'END.RANGE' , 'END.AZI' , 'WFO' , 'STATEOFFIC')
 
-data <- data[,!(names(data) %in% drop.columns)]
+#data <- data[,!(names(data) %in% drop.columns)]
 
 
 ```
+
+## Investigations:
+- Across the United States, which types of events (as indicated in the EVTYPE variable) are most harmful with respect to population health?
+
+```{r}
+# aggregate fatailities by event type
+human.life <- aggregate(FATALITIES ~ EVTYPE , data = data , FUN = sum)      
+
+# aggregate injuries by event type
+human.injury <- aggregate(INJURIES ~ EVTYPE , data = data , FUN = sum) 
+ 
+# merge data sets
+human.health <- merge(human.life , human.injury , by = 'EVTYPE')           
+
+# order by 
+
+# select only events that represent injuries or fatailities greater than the mean
+human.health.trim <- subset(human.health , FATALITIES > mean(FATALITIES) | INJURIES > mean(INJURIES))  
+
+human.health.melt <- melt(human.health.trim , id = c('EVTYPE'))
+
+health.plot <- ggplot(data = human.health.melt , aes(x = EVTYPE , y = value , fill = variable)) +
+                    geom_bar(stat = 'identity' , position = position_dodge() , color = 'black') +  
+                    ggtitle("Storm Impact to Human Health") + 
+                    theme(plot.title = element_text(lineheight=.8, face="bold")) + 
+                    theme(axis.text.x=element_text(angle=60, hjust=1, size = 8)) + 
+                    labs(x = 'Event Type' , y = 'Total Incidents 1950 - NOV 2011')
+
+print(health.plot)
+
+```
+
+- Across the United States, which types of events have the greatest economic consequences?
+
 
 ##  Results
 Present results
